6b5f9b88aad152ef05f23bd092d0db0c8d2fb326
sahithigrandhi <sahithi.grandhi13@gmail.com>
2017-05-28 17:02:13 -0500

    setting up hibernate

---
+++ UserDetails.java
@@ -0,0 +1,37 @@
+package edu.learningHibernate.dto;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity //(name="User_Details")
+public class UserDetails {
+	@Id
+	private int user_Id;
+	private String userName;
+	
+
+	public UserDetails(int user_Id, String userName) {
+		super();
+		this.user_Id = user_Id;
+		this.userName = userName;
+	}
+	
+	public UserDetails() {
+		super();
+	}
+
+	public int getUser_Id() {
+		return user_Id;
+	}
+	public void setUser_Id(int user_Id) {
+		this.user_Id = user_Id;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+
+}
---
+++ UserDetailsTest.java
@@ -0,0 +1,28 @@
+package edu.learningHibernate.test;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import edu.learningHibernate.dto.UserDetails;
+
+public class UserDetailsTest {
+
+	public static void main(String[] args) {
+		UserDetails user1=new UserDetails();
+		user1.setUser_Id(3);
+		user1.setUserName("first user");
+
+		UserDetails user2=new UserDetails(2,"second user");
+
+		SessionFactory sessionfactory=new Configuration().configure().buildSessionFactory();
+		Session session=sessionfactory.openSession();
+		session.beginTransaction();
+		//session.save(user1);
+		session.delete(user2);
+		//session.save(user2);
+		session.getTransaction().commit();
+		System.out.println("_______________________");
+	}
+
+}
